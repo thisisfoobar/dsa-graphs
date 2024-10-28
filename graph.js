e761183eae265ae491c4c981e8c1721721a4bb82
thisisfoobar <matt.mcelreath26@gmail.com>
2024-10-28 16:18:38 -0400

    Completed functions

--- graph.js
+++ graph.js
@@ -11,25 +11,70 @@ class Graph {
   }
 
   // this function accepts a Node instance and adds it to the nodes property on the graph
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   // this function accepts an array of Node instances and adds them to the nodes property on the graph
-  addVertices(vertexArray) {}
+  addVertices(vertexArray) {
+    vertexArray.forEach(vertex => this.nodes.add(vertex));
+  }
 
   // this function accepts two vertices and updates their adjacent values to include the other vertex
-  addEdge(v1, v2) {}
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   // this function accepts two vertices and updates their adjacent values to remove the other vertex
-  removeEdge(v1, v2) {}
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   // this function accepts a vertex and removes it from the nodes property, it also updates any adjacency lists that include that vertex
-  removeVertex(vertex) {}
+  removeVertex(vertex) {
+    this.nodes.delete(vertex);
+    this.nodes.forEach(node => node.adjacent.delete(vertex));
+  }
 
   // this function returns an array of Node values using DFS
-  depthFirstSearch(start) {}
+  depthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+
+    const dfs = (node) => {
+        if (!node || visited.has(node)) return;
+        visited.add(node);
+        result.push(node.value);
+        node.adjacent.forEach(neighbor => dfs(neighbor));
+    };
+
+    dfs(start);
+    return result;
+  }
 
   // this function returns an array of Node values using BFS
-  breadthFirstSearch(start) {}
+  breadthFirstSearch(start) {
+    const visited = new Set();
+    const result = [];
+    const queue = [start];
+    visited.add(start);
+
+    while (queue.length) {
+        const current = queue.shift();
+        result.push(current.value);
+
+        current.adjacent.forEach(neighbor => {
+            if (!visited.has(neighbor)) {
+                visited.add(neighbor);
+                queue.push(neighbor);
+            }
+        });
+    }
+
+    return result;
+  }
 }
 
 module.exports = {Graph, Node}
